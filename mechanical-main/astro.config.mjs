32053813b3663b41b8bd05270e3c4ae2f45d4bc1
jose3rodriguez <jtrjr@uwm.edu>
2024-07-13 17:20:15 -0500

    i think i see whats going on

--- mechanical-main/astro.config.mjs
+++ mechanical-main/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jose3rodriguez.github.io',
-    // base: 'jose3rodriguez.github.io'      
+    base: 'jose3rodriguez.github.io/mechanical-main'      
 });

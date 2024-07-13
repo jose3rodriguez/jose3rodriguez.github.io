c1891db32ed37c2acda8a738f3c81f66895692f4
jose3rodriguez <jtrjr@uwm.edu>
2024-07-13 14:53:45 -0500

    nojekyll

--- mechanical-main/astro.config.mjs
+++ mechanical-main/astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    integrations: [
+        preact(),
+        react(),
+        tailwind()],
+
+        site: 'https://jose3rodriguez.github.io',
+
+        output: 'static',
+        outDir: './docs', 
+        build: {
+            assets: 'astro'
+        }
+});

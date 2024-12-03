13f00121ece84b646a39647833f40e95c6c7c7d9
jose3rodriguez <jtrjr@uwm.edu>
2024-12-03 15:07:22 -0600

    setting up

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://jose3rodriguez.github.io',
-  base: 'jose3rodriguez.github.io',
+  base: 'jose3rodriguez.github.io/src/pages',
 
 });

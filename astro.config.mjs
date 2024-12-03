ad3f807a3bb3bee3ff0f2c2a0afb6cea46e69a56
jose3rodriguez <jtrjr@uwm.edu>
2024-12-03 14:34:48 -0600

    setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://jose3rodriguez.github.io',
+  site: 'https://jose3rodriguez.github.io',
+  base: '/jose3rodriguez.github.io', // If your site is hosted in a repository other than the root URL
+  output: 'static',
+  outDir: './docs', // This is where your static files will be generated
 });

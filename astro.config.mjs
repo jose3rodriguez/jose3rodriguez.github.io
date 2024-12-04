f1cc76f483814cdbfed3f54cb395813ced2f5da9
jose3rodriguez <jtrjr@uwm.edu>
2024-12-04 14:55:00 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://jose3rodriguez.github.io',
-<<<<<<< HEAD
-  base: 'jose3rodriguez.github.io',
-=======
   base: '/jose3rodriguez.github.io/src/pages',
-
->>>>>>> gh-pages
 });

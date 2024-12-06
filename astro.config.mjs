89bb1aa2267898982f8efaa82e4696ea29ccc165
jose3rodriguez <jtrjr@uwm.edu>
2024-12-06 13:07:33 -0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://jose3rodriguez.github.io/',
-  base: '/jose3rodriguez.github.io/', // If your repo is named the same as your username, this can be empty
   output: 'static',
   outDir: './dist',
 });
